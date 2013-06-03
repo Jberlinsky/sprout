85f94c48d28ec15f724be3085338f0fb85afc4a5
Jason Berlinsky <jberlinsky@gmail.com>
2013-06-03 13:46:02 -0500

    Skip Vim steps if already installed

--- pivotal_workstation/recipes/vim.rb
+++ pivotal_workstation/recipes/vim.rb
@@ -6,7 +6,7 @@ include_recipe "pivotal_workstation::homebrew"
 execute "install macvim and use its vim as system vim" do
   command "brew install macvim --override-system-vim"
   user node['current_user']
-  not_if "brew list | grep '^macvim$'"
+  not_if { File.exists?("/Applications/MacVim.app") }
 end
 
 execute "link macvim into place" do
@@ -15,11 +15,12 @@ execute "link macvim into place" do
   # "verify that command-t is correctly compiled" task in the vim_config recipe.
   command "ln -s /usr/local/Cellar/macvim/*/MacVim.app /Applications/MacVim.app"
   user node['current_user']
-  not_if { File.symlink?("/Applications/MacVim.app") }
+  not_if { File.exists?("/Applications/MacVim.app") }
 end
 
 execute "test to see if macvim link worked" do
   command "test -L /Applications/MacVim.app"
+  not_if { File.exists?("/Applications/MacVim.app") }
 end
 
 pivotal_workstation_bash_it_custom_plugin "vim-alias_vi_to_minimal_vim.bash"
